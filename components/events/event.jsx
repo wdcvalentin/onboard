92b931f4c96330fefd89692a7fd29be34322ab3c
Romain Valla <romainvalla@icloud.com>
2022-01-21 14:58:20 +0100

    Add images

--- components/events/event.jsx
+++ components/events/event.jsx
@@ -3,15 +3,30 @@ import { FiUsers, FiMapPin, FiClock, FiFileText } from 'react-icons/fi'
 
 export const Event = () => {
   return (
-    <div className={"event--card"}>
-      <div className={"event--card-wrapper"}>
-        <div className={"event--info"}>
-          <h3 className={"event--title"}>Italian Restaurant</h3>
-          <p className={"event--place"}><FiMapPin />La Table des Artistes Courbevoie 92</p>
-          <p className={"event--time"}><FiClock />de <strong>12:30pm</strong> à <strong>2pm</strong></p>
-          <p className={"event--description"}><FiFileText />The IT, Management and Commercial Development team joined this space</p>
-          <p className={"event--participant"}><FiUsers /> See all participants </p>
-          <button className={"event--cta"}>Join</button>
+    <div className={"card"}>
+      <div className={"card--container"}>
+        <div className={"card--content"}>
+          <div className={"card--heading"}>
+            <h3>Italian Restaurant</h3>
+            <p><FiMapPin /> La Table des Artistes Courbevoie 92</p>
+          </div>
+          <div className={"card--info"}>
+            <p><FiClock /> de <strong>12:30pm</strong> à <strong>2pm</strong></p>
+            <p><FiFileText /> The IT, Management and Commercial Development team joined this space</p>
+            <p><FiUsers /> See all participants </p>
+          </div>
+          <div className={"card--cta"}>
+            <button className={"event--cta"}>Join</button>
+          </div>
+        </div>
+        <div className={"card--image"}>
+          <Image
+            src={"/static/images/italian-restaurant.jpg"}
+            alt={""}
+            width={"100%"}
+            height={"100%"}
+            layout={"responsive"}
+          />
         </div>
       </div>
     </div>
