9f834a5742e1ee2c9ec3b03e213b22a8e4b484cb
Waché de Corbie Valentin <wdcvalentin@yahoo.fr>
2022-07-13 09:42:37 +0200

    feat(middleware): protecting dashboard route

--- app/middleware.js
+++ app/middleware.js
@@ -1,8 +1,15 @@
 import { NextResponse } from 'next/server'
+import { getToken } from "next-auth/jwt"
 
-export function middleware(request) {
-    console.log('[middleware]')
+export async function middleware(req) {
+    console.log('[middleware] Protection route')
+    const session = await getToken({ req: req, secret: process.env.NEXTAUTH_SECRET });
 
+    if (!session) {
+        return NextResponse.redirect('http://localhost:3000/login')
+    }
+
+    return NextResponse.next()
 }
 
 // Supports both a single string value or an array of matchers
