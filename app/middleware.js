e6a44c53c72ca6649cff4d12281fdfe6bacaa322
Waché de Corbie Valentin <wdcvalentin@yahoo.fr>
2022-07-17 11:39:18 +0200

    fix(middleware): fix middleware redirection login

--- app/middleware.js
+++ app/middleware.js
@@ -3,11 +3,10 @@ import { getToken } from "next-auth/jwt"
 
 export async function middleware(req) {
     console.log('[middleware] Protection route')
-    console.log(`production: ${process.env.NEXT_PUBLIC_NODE_ENV === 'production'}`)
     const session = await getToken({ req: req, secret: process.env.SECRET });
-    
+
     if (!session) {
-        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_NODE_ENV !== 'development' ? process.env.NEXTAUTH_URL : process.env.NEXT_PUBLIC_HOST_API_URL}/login`)
+        return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`)
     }
 
     return NextResponse.next()
