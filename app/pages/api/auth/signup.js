cd183c2a2535396dadcedd4d0a5316d85edfb449
Waché de Corbie Valentin <wdcvalentin@yahoo.fr>
2022-07-28 21:02:20 +0200

    fix(signup): add company to user

--- app/pages/api/auth/signup.js
+++ app/pages/api/auth/signup.js
@@ -1,6 +1,7 @@
 import { connectMongo } from "../../../utils/connectMongo";
 import { registerValidation } from "../../../utils/validation";
 import User from "../../../models/user";
+import Company from "../../../models/company";
 import bcrypt from 'bcryptjs';
 
 export default async function handler(req, res) {
@@ -22,13 +23,21 @@ export default async function handler(req, res) {
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const companyId = '61e92d2df366382904c39d4e'
         const user = new User({
             email: req.body.email,
             password: hashedPassword,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
+            company: companyId
         })
-        await user.save()
+
+        await Promise.all([
+            Company.findOneAndUpdate({ _id: companyId }, { $push: { workers: { _id: user._id } } }),
+            user.save()
+        ])
+
+
 
         return res.send(true)
     } catch (error) {
--- app/utils/dateFormat.js
+++ app/utils/dateFormat.js
@@ -2,7 +2,7 @@ export const formatDateHM = (date) => {
     const hour = new Date(date).getHours();
     const min = new Date(date).getMinutes();
 
-    return `${hour}:${min}`
+    return `${hour}:${min < 10 ? `0${min}` : min}`
 }
 
 export const formatDateToYMD = (date) => date.toISOString().slice(0, 10);
